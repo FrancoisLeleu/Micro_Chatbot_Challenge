61a865c271bd721c7628eb33d39d17aecb042a61
François Leleu <francois.leleu@users.noreply.github.com>
2021-02-15 16:49:03 +0100

    first step

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-/*------first step-------------*/
+/*-------------------------------------------------------------------first step------------------------------------------------------------*/
 
 const welcome = ["Hi, welcome", "Nice to see you again","Welcome back"];
 
@@ -7,3 +7,12 @@ let randomWelcome = welcome[Math.floor(Math.random()*welcome.length)];
 let p = document.getElementById("randomText");
 
 p.textContent = randomWelcome;
+
+/*------------------------------------------------------------How do you feel today----------------------------------------------------------*/
+
+function howare() {
+    setTimeout(function(){ alert("How do you feel today"); }, 6000);
+  }
+
+window.onload=howare
+
