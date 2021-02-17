b86be2d889bcd1387f0d6fafeaae4699f1312dda
François Leleu <francois.leleu@users.noreply.github.com>
2021-02-17 12:15:38 +0100

    avancement

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Micro_Chatbot_Challenge
 This is our ChatBot Challenge
+
+Workers on this project are :
+=============================
+- Sylvain Haenen : Javascript
+- François Leleu : Project Manager and Javascript
+- Sandrine Bataganira : Javascript
+- Sara brzn : Html and CSS
+
+We have implemented the following functions to our bot :
+========================================================
+- on loading of the page, the bot welcomes the user with a random greeting message, then, he ask the user "how do you feel today?".
+- The user can reply freely, and the bot need to check in the response if he can find a "yes" or a "no":
+- if the script find a "yes" in the message, the bot answers with a random sentence from a specific set of three
+- if the script find a "no" in the message, the bot answers with a random sentence from a specific set of three
+- if the script cannot find a "yes" or a "no" in the message, the bot answer "ACTUAL ANSWER"
+- if the script cannot find a "yes" or a "no" in the message after three attempts, the bot begins to be mad and use "ACTUAL ANSWER"
+- After this answer, the bot has finish his routine. He says "ACTUAL ANSWER" to the user and the textarea go to disabled mode.
+
+For the verifications :
+
+=======================
+The HTML and CSS pass the W3C Validation tool with a score of :
+
+The lighthouse test scores is : on Performance,  on Best Practices, on Accessibility and SEO.
--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-/*-----------------------------------------------------------------first step----------------------------------------------------------------------------------------*/
+
+/*-----------------------------------------------------------------first step----------------------------------------------------------------------------------------
 
 const welcome = ["Hi, welcome !", "Nice to see you again !","Welcome back !"];
 
@@ -7,7 +8,7 @@ const welcome = ["Hi, welcome !", "Nice to see you again !","Welcome back !"];
         let p = document.getElementById("greetings");
         p.textContent = message;
     }
-/*------------------------------------------------------------How do you feel today ?------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------How do you feel today ?------------------------------------------------------------------------------------
 
     window.onload= () => {
         displayMsg (randomItem);
@@ -15,9 +16,9 @@ const welcome = ["Hi, welcome !", "Nice to see you again !","Welcome back !"];
             displayMsg ("How Are You Today ?")}, 6000);
     }
     
-/*-------------------------------------------------------------------Yes or No-----------------------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------Yes or No-----------------------------------------------------------------------------------------
 
-/*---------------------------------------------------------Let and Constants assignations---------------------*/
+---------------------------------------------------------Let and Constants assignations---------------------
 
       let answerYes = ["I'm so Happy for you, you get 1 Life", "Press Start to begin a New Game", "Nooice, but it's still gonna be 20cts to Play"];
 
@@ -29,7 +30,7 @@ const welcome = ["Hi, welcome !", "Nice to see you again !","Welcome back !"];
 
       const randomNo = answerNo [Math.floor(Math.random()*answerNo.length)];
 
-/*-----------------------------------------------------------------Main Function----------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------Main Function----------------------------------------------------------------------------------------
 
     function sendInput() {
         let userInput = document.getElementById("input").value;
@@ -45,3 +46,79 @@ const welcome = ["Hi, welcome !", "Nice to see you again !","Welcome back !"];
             };
             
           }};
+
+
+
+          
+/*-----------------------------------------------------------------------3 Messages random de Salutation-------------------------------------------------------------------------*/
+
+const welcome = ["Hi, welcome !", "Nice to see you again !","Welcome back !"];
+
+
+    var randomItem = welcome[Math.floor(Math.random()*welcome.length)];
+    function displayMsg (message) {
+        let p = document.getElementById("greetings");
+        p.textContent = message;
+    }
+    window.onload= () => {
+        displayMsg (randomItem);
+/*----------------------------------------------------------------------"HOW ARE YOU TODAY" ROBOT MESSAGE----------------------------------------------------------------------*/
+        setTimeout(function () { 
+            displayMsg ("How Are You Today ?")}, 4000);
+    } 
+
+/*------------------------------------------------------------------------------Réponse du USER---------------------------------------------------------------------------------*/
+
+    function getInputValue() {
+    return document.getElementById("input").value;
+  }
+
+/*----------------------------------------------------------------------------Réactions du Bot ----------------------------------------------------------------------------------*/
+
+let counterOfClick = 0;
+
+
+document.getElementById("send").addEventListener("click", (e) => {
+    counterOfClick++
+    let y = "yes"
+    let n = "no"
+  
+    if (getInputValue ().toLowerCase().includes(y)){
+  
+          const yesAnswers = ["I'm Happy for You !", "Enjoy my friend, ENJOY !...", "Good to Hear !"]
+  
+          var randomItem = yesAnswers[Math.floor(Math.random()*yesAnswers.length)];
+          displayMsg (randomItem);
+  
+    } else if (getInputValue ().toLowerCase().includes(n)){
+  
+          const noAnswers = ["Don't worry, things are gonna get easier !", "Sad life ; sad destiny !", "Life isn't easy for teddy bears like us... "]
+          
+          var randomItem = noAnswers[Math.floor(Math.random()*noAnswers.length)];
+          displayMsg (randomItem);
+  
+    } else {
+  
+          if(counterOfClick >= 3){
+
+            displayMsg("Bzzzt bzzt my circuits are boiling");
+            setTimeout(function () {
+                shutOffMachine ()
+            }, 3000)
+
+          } else {
+              
+            displayMsg ("I didn't get that, could you talk proper english please ?");
+
+          }
+  
+    }
+})
+
+function shutOffMachine () {
+
+    document.getElementById("input").disabled=true;
+    document.getElementById("send").disabled=true;
+    displayMsg ("Bye !");
+    
+}
