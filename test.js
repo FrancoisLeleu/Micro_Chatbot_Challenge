13402666cc9896e7a5c6122833703f9c78d0ed0b
sandrine <sandrine.bataganira@gmail.com>
2021-02-17 16:01:53 +0100

    last version

--- test.js
+++ test.js
@@ -1,62 +1,93 @@
-// welcome messages 
+// ---------welcome messages---------------- //
+
+const welcome = ["Hi, welcome !", "Nice to see you again !","Welcome back !"];
 
 
-const phrases =[
-    "hi welcome",
-    " Nice to see you again",
-    "welcome back",
-]
-}
-// html
-/*let phrasesindex =(Math.floor(Math.random()*phrases.length));
-
-console.log( phrases[phrasesindex]);
-*/
-// 3 welcomes random messages .
-
-// chat bot
-
-/*let bot = prompt ("how do you feel today");*/
-
-// inclure html (récuperer doc)
-//link 
-// var useranswer(si contient oui ou non)
-// getElementById (ENTER BUTTON)
-document.getElementById("id").addEventListener('click', ());{
-const answers =[
-    ("blabla", "blalbla", "lolololo"),
-    ("hahah", "gthtgth", "hdtyrike"),
-    ("hdgfty"," nghteek","nbhgur"), 
-]
-const answer =[
-    ("nonon", "niet ", "no"),
-    ("non", "hoya", "niecth"),
-    ("niiiet"," nononno","nono"), 
-]
-
-var reponse ={Y:"yes", n:"non"}; 
-
-for (var property in reponse){
-    if ("yes") {
-        let answersindex = Math.floor(Math.random()*answers.length);
-        console.log(answers[answersindex]);
-}
-    else if ("no"){
-        let answerindex = Math.floor(Math.random()*answer.length);
-        console.log(answer[answerindex]);
-}
-    else { 
-        document.getElementsById("welcome");
-        element.innerHTML = "mad";
-            console.log("i didn't understand, can you please try again");
-        
-        if ( (reponse != "yes" && reponse !="non") =3  ){
-            console.log ("dude, are you sure you can read?");
-        } // alert
+   
+    var randomItem = welcome[Math.floor(Math.random()*welcome.length)]; // randommessage
+    function displayMsg (message) {  // fonction réutilisable(displayMsg)!
+        let p = document.getElementById("randomText");
+        p.textContent = message;
     }
+    
+    window.onload= () => {
+        displayMsg (randomItem);
+        setTimeout(function () { 
+        displayMsg ("How Are You Today ?")}, 6000);
+    }
+    //----------- Second part( user answer)----------//
+    
+    const yes =[
+            "good" , "fine" , "great"
+        ]
+    let sentenceYes =yes[Math.floor(Math.random()*yes.length)];
+
+    const no =[
+            "just smile", "that's too bad" , "don't worry" 
+        ]
+    let sentenceNo =no[Math.floor(Math.random()*no.length)];
+        
+    let answerY= "yes";
+    let answerN="no";
+    
+    var welcomebot = document.getElementById("welcomeBot");
+    var frustrated = document.getElementById("frustratedBot");
+    var mad = document.getElementById("madBot");
+    //HIDDEN IMAGES
+    frustratedBot.style.display = "none";
+    madBot.style.display = "none";
+
+
+function sendInput() {  
+    let userInput = document.getElementById("input").value;// récuperer la valeur 
+
+// include() or touppercase/tolowercase(): rajoute plus de condition dans la selection des rép !
+
+
+    
+    if (userInput== answerY){
+        displayMsg(sentenceYes);
+        welcomeBot.style.display="inline";
+        frustrated.style.display = "none";
+        mad.style.display = "none";
+
+
+    }
+    else if (userInput==answerN){
+        displayMsg(sentenceNo);
+        welcomeBot.style.display="none";
+        frustrated.style.display = "inline";
+        mad.style.display = "none";
+    }
+    else {
+        displayMsg("dude, are you sure you can read?") ;
+        welcomeBot.style.display="none";
+        frustrated.style.display = "none";
+        mad.style.display = "inline";
+        
+document.getElementById("send").addEventListener('click',()=>{  
+    xClick++        
+    
+                let xClick = 0 ;
+                if(xClick > 3){
+                    displayMsg("what are you doing");
+                }
+                else {
+                        setTimeout(function(){
+                        displayMsg("goodbye")},5000);
+                }
+            
+             });// close function
+        
+       
+        }
 }
 
-}
+
+ 
+   
+      
+ 
 
 
 
