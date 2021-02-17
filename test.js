d685bdd38884958f981b19fd2f1c63f66c442837
sarabrzn <sara.brazuna.tavares@gmail.com>
2021-02-17 09:27:35 +0100

    commit

--- test.js
+++ test.js
@@ -1,91 +1,48 @@
-// WELCOME MESSAGES 
-//CREATE ARRAY FOR WELCOME MESSAGES
-/* const welcome = ["Hi, welcome", "Nice to see you again","Welcome back"]; */
-//RANDOMIZE ARRAY ITEMS
-/* let randomWelcome = welcome[Math.floor(Math.random()*welcome.length)]; */
-//RETRIEVE MARKUP FOR BOT WELCOME MESSAGE   
-/* let p = document.getElementById("randomText"); */
-//ATTRIBUTE RANDOM MESSAGE FROM ARRAY
-/* p.textContent = randomWelcome; */
-//"HOW DO YOU FEEL TODAY?"
-
-/* setTimeout(function(){
-    document.getElementById("randomText").innerHTML = "How do you feel today?";},3000);
- */
-    const welcome = ["Hi, welcome", "Nice to see you again","Welcome back"];
-
-    var randomItem = welcome[Math.floor(Math.random()*welcome.length)];
-      
-    function displayMsg (message) {
-      let p = document.getElementById("greetings");
-      p.textContent = message;
-      }
-  
-    window.onload= () => {
-      displayMsg (randomItem);
-      setTimeout(function () { 
+//IMAGES
+welcomeBot = document.getElementById("welcome");
+frustratedBot = document.getElementById("frustrated");
+madBot = document.getElementById("mad");
+//HIDDEN IMAGES
+frustratedBot.style.display = "none";
+madBot.style.display = "none";
+//WELCOME MESSAGES(AUTOMATIC)
+const welcome = ["Hi, welcome !", "Nice to see you again !","Welcome back !"];
+var randomItem = welcome[Math.floor(Math.random()*welcome.length)];
+//DISPLAY MESSAGES
+function displayMsg (message) {
+    let p = document.getElementById("greetings");
+    p.textContent = message;
+}
+window.onload= () => {
+    displayMsg (randomItem);
+    setTimeout(function () { 
         displayMsg ("How Are You Today ?")}, 6000);
-      };
-  
+}
+//YES AND NO MESSAGES(DEPEND ON INTERACTION)
+const answerYes = ["I'm so Happy for you, you get 1 Life", "Press Start to begin a New Game", "Nooice, but it's still gonna be 20cts to Play"];
+const answerNo = ["You'll start with minus 1 Life", "What's gonna be when you loose the Game ?", "So sad for you...can we begin now ?"];
+var randomYes = answerYes [Math.floor(Math.random()*answerYes.length)];
+var randomNo = answerNo [Math.floor(Math.random()*answerNo.length)];
+//SEND MESSAGES 
+
+function sendInput() {
     
-// SEND BUTTON
-/* let btn = document.getElementById("send"); */
-/* btn.addEventListener("click",function{ */
-//function to send
-/* }) */
-
-
-
-
-/* let y = "yes";
-let n = "no";
-
-if (userInput.match(y) != null) {
-    p.textContent = randomYes;
-}else if (userInput.match(n)!= null){
-  p.textContent = randomNo;
-} */
-
-
-
-
-
-// inclure html (récuperer doc)
-//link 
-// var useranswer(si contient oui ou non)
-// getElementById (ENTER BUTTON)
-const answers =[
-    ("blabla", "blalbla", "lolololo"),
-    ("hahah", "gthtgth", "hdtyrike"),
-    ("hdgfty"," nghteek","nbhgur"), 
-]
-const answer =[
-    ("nonon", "niet ", "no"),
-    ("non", "hoya", "niecth"),
-    ("niiiet"," nononno","nono"), 
-]
-
-var reponse ={Y:"yes", n:"non"}; 
-
-for (var property in reponse){
-    if ("yes") {
-        let answersindex = Math.floor(Math.random()*answers.length);
-        console.log(answers[answersindex]);
-}
-    else if ("no"){
-        let answerindex = Math.floor(Math.random()*answer.length);
-        console.log(answer[answerindex]);
-}
-    else { 
-        console.log("i didn't understand, can you please try again");
-        if ( (reponse != "yes" && reponse !="non") =3  ){
-            console.log ("dude, are you sure you can read?");
-        } // alert
+    let userInput = document.getElementById("input").value;
+    if (userInput == "yes") {
+        displayMsg (randomYes);     
+    } else if (userInput == "no") {
+        displayMsg (randomNo); 
+    } else {
+        welcomeBot.style.display="none";
+        frustratedBot.style.display = "inline";
+        displayMsg (" Whut ? I did not understand, can you please try again ?");
     }
+    
 }
 
-
-
-
+//DISABLE TEXTAREA AND BUTTON
+/* document.getElementById("input").disabled = true;
+document.getElementById("send").disabled = true; 
+  */
 
 
